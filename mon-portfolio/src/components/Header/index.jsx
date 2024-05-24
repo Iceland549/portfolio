1f8c91e6cd4134a95d0c756fdb4520ffa50747ca
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-05-24 15:00:13 +0200

    Delete img tag from 'Header' function

--- mon-portfolio/src/components/Header/index.jsx
+++ mon-portfolio/src/components/Header/index.jsx
@@ -6,10 +6,6 @@ function Header() {
         <header>
             <nav className="main-nav">
                 <NavLink className="main-nav-logo" to="/">
-                    <img
-                        className="main-nav-logo-image"
-
-                    />
                     <h1 className="main-title">Mon Portfolio</h1>
                 </NavLink>
             </nav>
