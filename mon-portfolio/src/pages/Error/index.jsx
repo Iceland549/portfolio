58be67122280a1b22970a9afb09302f46611f7b5
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-05-24 14:33:58 +0200

    Add article tag to 'Error' function

--- mon-portfolio/src/pages/Error/index.jsx
+++ mon-portfolio/src/pages/Error/index.jsx
@@ -4,12 +4,14 @@ import { Link } from 'react-router-dom';
 
 function Error() {
     return (
-        <div className='error-container'>
+        <div className='error'>
             <h1>404</h1>
-            <p>Oups ! La page que <br /> vous demandez n&apos;existe pas.</p>
-            <Link to={'/'}>
-                <p className='error-link'> Retour sur la page d&apos;accueil</p>
-            </Link>
+            <article>
+                <p>Oups ! La page que <br /> vous demandez n&apos;existe pas.</p>
+                <Link to={'/'}>
+                    <p className='error-link'> Retour sur la page d&apos;accueil</p>
+                </Link>
+            </article>
         </div>
     )
 }
