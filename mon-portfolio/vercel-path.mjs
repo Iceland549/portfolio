3e8f6f3f24d53e2bf72757cd4602b5dadcd6beca
Iceland549 <chaudenisbreiz@yahoo.fr>
2024-06-06 12:54:02 +0200

    Delete files

--- mon-portfolio/vercel-path.mjs
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const mainFolderPath = path.resolve(__dirname);
-
-console.log(mainFolderPath);
