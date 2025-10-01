caab53d79562ff977f896b2b47086200c85188ee
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-10-01 15:06:50 +0200

    Enhance Modal component to display technologies and highlights

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,22 +1,46 @@
 /* eslint-disable react/prop-types */
 import ReactModal from 'react-modal';
 
-const Modal = ({ isOpen, isClose, title, description, link }) => {
-    return (
-        <ReactModal
-            isOpen={isOpen}
-            isClose={isClose}
-            contentLabel={title}
-            className="modal-content"
-        >
-            <h2>{title}</h2>
-            {description.map((paragraph, index) => (
-                <p key={index}>{paragraph}</p>
-            ))}
-            <p>Lien GitHub : <a href={link} target="_blank" rel="noopener noreferrer" className='github-link'>{link}</a></p>
-            <button onClick={isClose}>Fermer</button>
-        </ReactModal>
-    );
+const Modal = ({ isOpen, isClose, title, description, technologies, highlights, link }) => {
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={isClose}
+      contentLabel={title}
+      className="modal-content"
+    >
+      <h2>{title}</h2>
+
+      {description.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+
+      {technologies && (
+        <div>
+          <h3>Technologies</h3>
+          <p>{technologies}</p>
+        </div>
+      )}
+
+      {highlights && (
+        <div>
+          <h3>Highlights</h3>
+          <p>{highlights}</p>
+        </div>
+      )}
+
+      {link && (
+        <p>
+          Lien GitHub :{' '}
+          <a href={link} target="_blank" rel="noopener noreferrer" className="github-link">
+            {link}
+          </a>
+        </p>
+      )}
+
+      <button onClick={isClose}>Fermer</button>
+    </ReactModal>
+  );
 };
 
 export default Modal;
