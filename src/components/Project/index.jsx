8274be247840d089ae747dc0f51780dbdc7f01d2
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-10-01 15:07:10 +0200

    Update Project component to pass full project data to modal

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 
-function Project({ title, description, openModal, link }) {
-    return (
-        <div className="project">
-            <h3 onClick={() => openModal(title, description, link)}>{title}</h3>
-        </div>
-    );
+function Project({ title, description, technologies, highlights, link, openModal }) {
+  return (
+    <div className="project">
+      <h3 onClick={() => openModal(title, description, technologies, highlights, link)}>
+        {title}
+      </h3>
+    </div>
+  );
 }
 
 export default Project;
