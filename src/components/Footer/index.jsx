139b55a99b4a5bd34d597d90471280f7daa3b0d9
Chau Denis <chaudenisbreiz@yahoo.fr>
2025-10-01 15:17:32 +0200

    Update copyright year in footer to reflect current year (2025)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ function Footer() {
             <div className="footer-content">
                 <p>CHAU Denis</p>
                 <p>iceland549@gmail.com</p>
-                <p>2024 Mon Portfolio</p>
+                <p>2025 Mon Portfolio</p>
             </div>
         </footer>
     );
